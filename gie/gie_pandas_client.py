3b4ec4c525becf65cc69b30020d7989b15956e8e
Petar Petrov <ppetrov@roiti.com>
2022-10-19 09:13:07 +0300

    added config, setup, license files

--- gie/gie_pandas_client.py
+++ gie/gie_pandas_client.py
@@ -21,26 +21,26 @@ class GiePandasClient(GieRawClient):
 
     # query which provide us all ALSI news OR a specific item (news for a concrete country)
     async def query_alsi_news_listing(
-        self, news_url_item: Optional[Union[int, str]] = None
+            self, news_url_item: Optional[Union[int, str]] = None
     ):
         json_result = await super().query_alsi_news_listing(news_url_item=news_url_item)
         return pd.DataFrame(json_result["data"])
 
     # query which provide us all ALSI news OR a specific item (news for a concrete country)
     async def query_agsi_news_listing(
-        self, news_url_item: Optional[Union[int, str]] = None
+            self, news_url_item: Optional[Union[int, str]] = None
     ):
         json_result = await super().query_agsi_news_listing(news_url_item=news_url_item)
         return pd.DataFrame(json_result["data"])
 
     # AGSI query which provides data for a specific country
     async def query_country_agsi_storage(
-        self,
-        country: Optional[Union[AGSICountry, str]] = None,
-        start: Optional[Union[datetime.datetime, str]] = None,
-        end: Optional[Union[datetime.datetime, str]] = None,
-        date: Optional[Union[datetime.datetime, str]] = None,
-        size: Optional[Union[int, str]] = None,
+            self,
+            country: Optional[Union[AGSICountry, str]] = None,
+            start: Optional[Union[datetime.datetime, str]] = None,
+            end: Optional[Union[datetime.datetime, str]] = None,
+            date: Optional[Union[datetime.datetime, str]] = None,
+            size: Optional[Union[int, str]] = None,
     ):
         json_result = await super().query_country_agsi_storage(
             country=country, start=start, end=end, date=date, size=size
@@ -50,12 +50,12 @@ class GiePandasClient(GieRawClient):
 
     # ALSI query which provides data for a specific country
     async def query_country_alsi_storage(
-        self,
-        country: Optional[Union[ALSICountry, str]] = None,
-        start: Optional[Union[datetime.datetime, str]] = None,
-        end: Optional[Union[datetime.datetime, str]] = None,
-        date: Optional[Union[datetime.datetime, str]] = None,
-        size: Optional[Union[int, str]] = None,
+            self,
+            country: Optional[Union[ALSICountry, str]] = None,
+            start: Optional[Union[datetime.datetime, str]] = None,
+            end: Optional[Union[datetime.datetime, str]] = None,
+            date: Optional[Union[datetime.datetime, str]] = None,
+            size: Optional[Union[int, str]] = None,
     ):
         json_result = await super().query_country_alsi_storage(
             country=country, start=start, end=end, date=date, size=size
@@ -65,12 +65,12 @@ class GiePandasClient(GieRawClient):
 
     # AGSI query which provides all the data for a specific facility
     async def query_agsi_facility_storage(
-        self,
-        facility_name: Union[AGSIFacility, str],
-        start: Optional[Union[datetime.datetime, str]] = None,
-        end: Optional[Union[datetime.datetime, str]] = None,
-        date: Optional[Union[datetime.datetime, str]] = None,
-        size: Optional[Union[int, str]] = None,
+            self,
+            facility_name: Union[AGSIFacility, str],
+            start: Optional[Union[datetime.datetime, str]] = None,
+            end: Optional[Union[datetime.datetime, str]] = None,
+            date: Optional[Union[datetime.datetime, str]] = None,
+            size: Optional[Union[int, str]] = None,
     ):
         json_result = await super().query_agsi_facility_storage(
             facility_name=facility_name, start=start, end=end, date=date, size=size
@@ -80,12 +80,12 @@ class GiePandasClient(GieRawClient):
 
     # ALSI query which provides all the data for a specific facility
     async def query_alsi_facility_storage(
-        self,
-        facility_name: Union[ALSIFacility, str],
-        start: Optional[Union[datetime.datetime, str]] = None,
-        end: Optional[Union[datetime.datetime, str]] = None,
-        date: Optional[Union[datetime.datetime, str]] = None,
-        size: Optional[Union[int, str]] = None,
+            self,
+            facility_name: Union[ALSIFacility, str],
+            start: Optional[Union[datetime.datetime, str]] = None,
+            end: Optional[Union[datetime.datetime, str]] = None,
+            date: Optional[Union[datetime.datetime, str]] = None,
+            size: Optional[Union[int, str]] = None,
     ):
         json_result = await super().query_alsi_facility_storage(
             facility_name=facility_name, start=start, end=end, date=date, size=size
@@ -95,12 +95,12 @@ class GiePandasClient(GieRawClient):
 
     # AGSI query which provides data for a specific company
     async def query_agsi_company(
-        self,
-        company_name: Union[AGSICompany, str],
-        start: Optional[Union[datetime.datetime, str]] = None,
-        end: Optional[Union[datetime.datetime, str]] = None,
-        date: Optional[Union[datetime.datetime, str]] = None,
-        size: Optional[Union[int, str]] = None,
+            self,
+            company_name: Union[AGSICompany, str],
+            start: Optional[Union[datetime.datetime, str]] = None,
+            end: Optional[Union[datetime.datetime, str]] = None,
+            date: Optional[Union[datetime.datetime, str]] = None,
+            size: Optional[Union[int, str]] = None,
     ):
         json_result = await super().query_agsi_company(
             company_name=company_name, start=start, end=end, date=date, size=size
@@ -110,12 +110,12 @@ class GiePandasClient(GieRawClient):
 
     # ALSI query which provides data for a specific company
     async def query_alsi_company(
-        self,
-        company_name: Union[ALSICompany, str],
-        start: Optional[Union[datetime.datetime, str]] = None,
-        end: Optional[Union[datetime.datetime, str]] = None,
-        date: Optional[Union[datetime.datetime, str]] = None,
-        size: Optional[Union[int, str]] = None,
+            self,
+            company_name: Union[ALSICompany, str],
+            start: Optional[Union[datetime.datetime, str]] = None,
+            end: Optional[Union[datetime.datetime, str]] = None,
+            date: Optional[Union[datetime.datetime, str]] = None,
+            size: Optional[Union[int, str]] = None,
     ):
         json_result = await super().query_alsi_company(
             company_name=company_name, start=start, end=end, date=date, size=size
@@ -124,11 +124,11 @@ class GiePandasClient(GieRawClient):
         return df
 
     async def query_agsi_unavailability(
-        self,
-        country: Optional[Union[AGSICountry, str]] = None,
-        start: Optional[Union[datetime.datetime, str]] = None,
-        end: Optional[Union[datetime.datetime, str]] = None,
-        size: Optional[Union[int, str]] = None,
+            self,
+            country: Optional[Union[AGSICountry, str]] = None,
+            start: Optional[Union[datetime.datetime, str]] = None,
+            end: Optional[Union[datetime.datetime, str]] = None,
+            size: Optional[Union[int, str]] = None,
     ):
         json_result = await super().query_agsi_unavailability(
             country=country, start=start, end=end, size=size
@@ -137,11 +137,11 @@ class GiePandasClient(GieRawClient):
         return df
 
     async def query_alsi_unavailability(
-        self,
-        country: Optional[Union[ALSICountry, str]] = None,
-        start: Optional[Union[datetime.datetime, str]] = None,
-        end: Optional[Union[datetime.datetime, str]] = None,
-        size: Optional[Union[int, str]] = None,
+            self,
+            country: Optional[Union[ALSICountry, str]] = None,
+            start: Optional[Union[datetime.datetime, str]] = None,
+            end: Optional[Union[datetime.datetime, str]] = None,
+            size: Optional[Union[int, str]] = None,
     ):
         json_result = await super().query_alsi_unavailability(
             country=country, start=start, end=end, size=size
