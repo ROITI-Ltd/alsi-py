e3b8b3a70821572604af4263d9b1b8b77d54a295
nttoshev <ntoshev@roiti.com>
2022-11-07 15:31:59 +0200

    (feat) Add enums for ALSI/AGSI APIs and utility lookup functions

---
+++ roitigie/mappings/api_mappings.py
@@ -0,0 +1,11 @@
+"""An API type enumerator"""
+import enum
+
+
+class APIType(str, enum.Enum):
+    """
+    Enumerator class for the two API types.
+    """
+
+    AGSI = "https://agsi.gie.eu/api/"
+    ALSI = "https://alsi.gie.eu/api/"
